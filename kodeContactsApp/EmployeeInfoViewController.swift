f0cd34443058170fbd73637a7128e9079df8c232
anevodchikova <anevodchikova29@gmail.com>
2023-02-20 15:11:53 +0300

    Добавлено приватное свойство для хранения информации о пользователе и метод для передачи данных из tableView

--- kodeContactsApp/EmployeeInfoViewController.swift
+++ kodeContactsApp/EmployeeInfoViewController.swift
@@ -9,5 +9,21 @@ import UIKit
 import SnapKit
 
 class EmployeeInfoViewController: UIViewController {
+    //MARK: Private properties
+    private var person: Person?
+    
+    func sendData(person: Person) {
+        self.person = person
+        print(person)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //создать кастомный navigation bar
+    }
+}
 
+extension EmployeeInfoViewController {
+    //MARK: Private methods
+    
 }
