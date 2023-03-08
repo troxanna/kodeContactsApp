66aaa95b8d1721aab744cba96919841108908f7e
anevodchikova <anevodchikova29@gmail.com>
2023-03-08 17:09:58 +0300

    Сделала UIView для всех типов ошибок, объект ошибки вынесла в ErrorModel, в инициализаторе ErrorView передаю конфигурацию ошибки

---
+++ kodeContactsApp/API/models/Error.swift
@@ -0,0 +1,15 @@
+//
+//  Error.swift
+//  kodeContactsApp
+//
+//  Created by Anastasia Nevodchikova on 08.03.2023.
+//
+
+import Foundation
+
+struct Error {
+    var title: String
+    var message: String
+    var imageName: String
+    var repeatRequest: Bool
+}
